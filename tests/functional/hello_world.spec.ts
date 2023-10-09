683dd970cd0cd3f4ded0bdff4fbedb2b7d5e544f
Gammelinne <kylian.renault@viacesi.fr>
2023-10-09 14:56:31 +0200

    first test

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -3,6 +3,5 @@ import { test } from '@japa/runner'
 test('display welcome page', async ({ client }) => {
   const response = await client.get('/')
 
-  response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  response.assertStatus(404)
 })
