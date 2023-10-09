3a28c6ee2baa1111baf34ebbea6762ccc0981e13
Gammelinne <kylian.renault@viacesi.fr>
2023-10-09 10:14:37 +0200

    ajout du readme

--- start/routes.ts
+++ start/routes.ts
@@ -17,12 +17,34 @@
 | import './routes/customer'
 |
 */
+
 import Route from '@ioc:Adonis/Core/Route'
-//import Database from '@ioc:Adonis/Lucid/Database'
+
+/*
+|--------------------------------------------------------------------------
+| Public routes
+|--------------------------------------------------------------------------
+|
+|   You don't need to be authenticated to access the routes below
+|
+*/
 
 // User routes
 Route.post('/register', 'UsersController.register')
 Route.post('/login', 'UsersController.login')
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated routes
+|--------------------------------------------------------------------------
+|
+|   You need to be authenticated to access the routes below
+|   To be authenticated, you need to send a header with the token you received when you logged in
+|   The token must be sent in the Authorization header
+|   Example of bearer token : NQ.VmvhsBgnHiK60tWff5xMtQTsKtJbXNeLtWDzue5qMQpyILe5FnElGzBJHTwD
+|   After add to the route the middleware('auth') to protect it
+|
+*/
 
 // Post routes
 Route.get('/posts', 'PostsController.index').middleware('auth')
